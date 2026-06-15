0b1dc68c274a660dd90d2fc5ba9fad52d913fdfb
EBF <fingerhut22@gmail.com>
2026-06-14 21:27:38 -0400

    Add `NullTradeRecord` and `TradeEntryViewModel` for improved trade entry handling

--- tests/forms/option_entry/test_trade_entry_form.py
+++ tests/forms/option_entry/test_trade_entry_form.py
@@ -1,4 +1,6 @@
 import pytest
+from ebf_trading.domain.value_objects.option_specific.option_type import OptionType
+from ebf_trading.domain.value_objects.positions.position_side import PositionSide
 
 from ebf_trading_ui.forms.option_entry.trade_entry_form import TradeEntryForm
 from ebf_trading_ui.view_models.ports.null_trade_record import NullTradeRecord
@@ -35,8 +37,39 @@ class TestTradeEntryForm:
                     sut.ui.position.setCurrentIndex(sut.ui.position.findData(LC))
                     assert sut.tracker.is_dirty
 
-    class TestWhenExistingLongPosition:
-        pass
+    class TestWhenExistingLongCall:
 
-    class TestWhenExistingShortPosition:
-        pass
+        @pytest.fixture
+        def sut(self, qtbot) -> TradeEntryForm:
+            record = NullTradeRecord(side=PositionSide.LONG, option_type=OptionType.CALL)
+            form = TradeEntryForm(record)
+            qtbot.addWidget(form)
+            return form
+
+        class TestPositionCombo:
+            class TestInitialState:
+
+                def test_widget_shows_long_call(self, sut):
+                    assert sut.ui.position.currentData() is LC
+
+                def test_model_is_long_call(self, sut):
+                    assert sut.model.position_spec is LC
+
+    class TestWhenExistingShortPut:
+
+        @pytest.fixture
+        def sut(self, qtbot) -> TradeEntryForm:
+            record = NullTradeRecord(side=PositionSide.SHORT, option_type=OptionType.PUT)
+            form = TradeEntryForm(record)
+            qtbot.addWidget(form)
+            return form
+
+        class TestPositionCombo:
+            class TestInitialState:
+
+                def test_widget_shows_short_put(self, sut):
+                    assert sut.ui.position.currentData() is SP
+
+                def test_model_is_short_put(self, sut):
+                    assert sut.model.position_spec is SP
+
